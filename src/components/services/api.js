6229e43413a0758309bc049a7ca81a1f83a9305d
jorgebp226 <jorgebp226@gmail.com>
2024-10-30 16:50:54 +0100

    Cambios en el singup 2

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,22 +1,13 @@
-import { getCurrentUser, fetchAuthSession } from 'aws-amplify/auth';
-
 const API_ENDPOINT = 'https://j1asmzdgbg.execute-api.eu-west-3.amazonaws.com/google-reviews/Preferecias';
 
 export const saveUserPreferences = async (preferences) => {
   try {
-    const user = await getCurrentUser();
-    const { idToken } = (await fetchAuthSession()).tokens;
-    
     const response = await fetch(`${API_ENDPOINT}/preferences`, {
       method: 'POST',
       headers: {
-        'Authorization': `Bearer ${idToken.getJwtToken()}`,
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({
-        sub: user.userId,
-        ...preferences
-      }),
+      body: JSON.stringify(preferences),
     });
 
     if (!response.ok) {
