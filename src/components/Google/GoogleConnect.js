636208b696199f74125770cd90fea9151490f017
jorgebp226 <jorgebp226@gmail.com>
2024-10-31 13:29:08 +0100

    Cambios en la app de google 2

--- src/components/Google/GoogleConnect.js
+++ src/components/Google/GoogleConnect.js
@@ -2,10 +2,9 @@
 import React from 'react';
 
 const GoogleConnect = () => {
-    const clientId = "179591697653-f9rkd82ajen3g9qn9tmhneck1gnh4of8.apps.googleusercontent.com";
+    const clientId = "273306591185-9opq5im08p95a8puikqk9lgltl91d76q.apps.googleusercontent.com";
     const redirectUri = "https://reviewsmanager.betalky.es/auth/callback";
-    const scope = "https://www.googleapis.com/auth/business.manage";
-
+    const scope = "https://www.googleapis.com/auth/business.manage https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
     const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&access_type=offline`;
 
     const handleConnectGoogle = () => {
